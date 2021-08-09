97f5c3e19f97d11a2a945e45e03d2ba6f52a864d
olyviah82 <77206032+olyviah82@users.noreply.github.com>
2021-08-09 04:10:14 +0300

    mainactivity

--- app/src/main/java/com/example/qrcode/MainActivity.java
+++ app/src/main/java/com/example/qrcode/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.qrcode;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -13,13 +14,19 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -28,13 +35,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-
-
-
-//    private EditText course;
-//    private EditText emailAddress;
-//    private EditText phoneNumber;
+public class MainActivity extends AppCompatActivity {
     private EditText dryCough;
     private EditText fever;
     private EditText fatigue;
@@ -43,23 +44,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private EditText taste;
     private EditText aches;
     private EditText contact;
-
-
-
     private Button submitBTN;
-
-
-    //Declare an Instance of reference where we have user details
-    private DatabaseReference mDatabaseUsers;
-
     private FirebaseUser mCurrentUser;
-
     private FirebaseAuth mAuth;
-
-
-
-    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-    private DatabaseReference databaseReference = firebaseDatabase.getReference().child("Screening Details");
+   // private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseref ;
+    private StorageReference mStorageRef;
+    private DatabaseReference mDatabaseUsers;
 
 
 
@@ -69,10 +60,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAuth = FirebaseAuth.getInstance();
-        mCurrentUser = mAuth.getCurrentUser();
-
-        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
+       // mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
 
 //        Spinner spinner
 //                =findViewById(R.id.course);
@@ -93,14 +81,16 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         aches = findViewById(R.id.aches);
         contact = findViewById(R.id.contact);
         submitBTN = findViewById(R.id.submitBTN);
-
-
-
+        mStorageRef=FirebaseStorage.getInstance().getReference();
+        databaseref=FirebaseDatabase.getInstance().getReference().child("Screening");
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+        mDatabaseUsers=FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
 
         submitBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+Toast.makeText(MainActivity.this,"posting ....",Toast.LENGTH_LONG).show();
                 java.util.Calendar calendar = Calendar.getInstance();
                 SimpleDateFormat currentData = new SimpleDateFormat("dd-MM-yyyy");
                 final String saveCurrentDate = currentData.format(calendar.getTime());
@@ -110,100 +100,63 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
                 final String saveCurrentTime = currentTime.format(calendar1.getTime());
 
-                if(TextUtils.isEmpty(dryCough.getText().toString())){
-                    Toast.makeText(MainActivity.this, "Kindly Enter Data Here", Toast.LENGTH_SHORT).show();
+                final String drycough= dryCough.getText().toString().trim();
+                final String Fever= fever.getText().toString().trim();
+                final String Fatigue= fatigue.getText().toString().trim();
+                final String Headache= headache.getText().toString().trim();
+                final String Sorethroat= soreThroat.getText().toString().trim();
+                final String Taste= taste.getText().toString().trim();
+                final String Aches= aches.getText().toString().trim();
+                final String Contact= contact.getText().toString().trim();
+                if(TextUtils.isEmpty(drycough)||TextUtils.isEmpty(Fever)||TextUtils.isEmpty(Fatigue)||TextUtils.isEmpty(Headache)||TextUtils.isEmpty(Sorethroat)||TextUtils.isEmpty(Taste)||TextUtils.isEmpty(Aches)||TextUtils.isEmpty(Contact)){
+
+                    Toast.makeText(MainActivity.this,"ALL FIELDS ARE REQUIRED",Toast.LENGTH_SHORT).show();
                 }
-                else {
-                    Toast.makeText(MainActivity.this, dryCough.getText().toString(),Toast.LENGTH_SHORT).show();
-                }
-                if(TextUtils.isEmpty(fever.getText().toString())){
-                    Toast.makeText(MainActivity.this, "Kindly Enter Data Here", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(MainActivity.this, fever.getText().toString(),Toast.LENGTH_SHORT).show();
-                }
-                if(TextUtils.isEmpty(fatigue.getText().toString())){
-                    Toast.makeText(MainActivity.this, "Kindly Enter Data Here", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(MainActivity.this, fatigue.getText().toString(),Toast.LENGTH_SHORT).show();
-                }
-                if(TextUtils.isEmpty(headache.getText().toString())){
-                    Toast.makeText(MainActivity.this, "Kindly Enter Data Here", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(MainActivity.this, headache.getText().toString(),Toast.LENGTH_SHORT).show();
-                }
-                if(TextUtils.isEmpty(soreThroat.getText().toString())){
-                    Toast.makeText(MainActivity.this, "Kindly Enter Data Here", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(MainActivity.this, soreThroat.getText().toString(),Toast.LENGTH_SHORT).show();
-                }
-                if(TextUtils.isEmpty(taste.getText().toString())){
-                    Toast.makeText(MainActivity.this, "Kindly Enter Data Here", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(MainActivity.this, taste.getText().toString(),Toast.LENGTH_SHORT).show();
-                }
-                if(TextUtils.isEmpty(aches.getText().toString())){
-                    Toast.makeText(MainActivity.this, "Kindly Enter Data Here", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(MainActivity.this, aches.getText().toString(),Toast.LENGTH_SHORT).show();
-                }
-                if(TextUtils.isEmpty(contact.getText().toString())){
-                    Toast.makeText(MainActivity.this, "Kindly Enter Data Here", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(MainActivity.this, contact.getText().toString(),Toast.LENGTH_SHORT).show();
+                else{
+                    final DatabaseReference newpost=databaseref.push();
+                    mDatabaseUsers.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            newpost.child("drycough").setValue(drycough);
+                            newpost.child("fever").setValue(Fever);
+                            newpost.child("fatigue").setValue(Fatigue);
+                            newpost.child("headache").setValue(Headache);
+                            newpost.child("soreThroat").setValue(Sorethroat);
+                            newpost.child("taste").setValue(Taste);
+                            newpost.child("aches").setValue(Aches);
+                            newpost.child("contact").setValue(Contact);
+                            newpost.child("uid").setValue(mCurrentUser.getUid());
+                            newpost.child("time").setValue(saveCurrentTime);
+                            newpost.child("date").setValue(saveCurrentDate);
+                            newpost.child("email").setValue(snapshot.child("email").getValue()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Intent intent=new Intent(MainActivity.this,Start2Activity.class);
+                                    startActivity(intent);
+
+                                }
+                            });
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull  DatabaseError error) {
+                            notifyUser("Database error: " + error.toException());
+                        }
+                    });
                 }
 
 
-
-                String dryCough1= dryCough.getText().toString();
-                String fever1 = fever.getText().toString();
-                String fatigue1 = fatigue.getText().toString();
-                String headache1 = headache.getText().toString();
-                String soreThroat1 = soreThroat.getText().toString();
-                String taste1 = taste.getText().toString();
-                String aches1 = aches.getText().toString();
-                String contact1 = contact.getText().toString();
-
-
-
-
-                HashMap<String, String> userMap = new HashMap<>();
-
-
-                userMap.put("Dry Cough", dryCough1);
-                userMap.put("Fever", fever1);
-                userMap.put("Fatigue", fatigue1);
-                userMap.put("Headache", headache1);
-                userMap.put("SoreThroat", soreThroat1);
-                userMap.put("Loss of Taste/Smell", taste1);
-                userMap.put("Body Aches", aches1);
-                userMap.put("Contact with confirmed case", contact1);
-
-                //generate a unique key for each data using push method
-                databaseReference.push().setValue(userMap);
-                Toast.makeText(MainActivity.this, "Data inserted", Toast.LENGTH_SHORT).show();
 
 
             }
         });
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(),text, Toast.LENGTH_SHORT).show();
+    private void notifyUser(String s) {
+        Toast.makeText(MainActivity.this,s,
+                Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
 }
 
 
