7c6bbdfa99421d4870e82a959d61b2db95aae344
olyviah82 <77206032+olyviah82@users.noreply.github.com>
2021-08-20 12:02:26 +0300

    screening details in the database

---
+++ app/src/main/java/com/example/qrcode/Screening.java
@@ -0,0 +1,99 @@
+package com.example.qrcode;
+
+public class Screening {
+    private String drycough;
+    private String Fever;
+    private String Fatigue;
+    private String Headache;
+    private String Sorethroat;
+    private String Taste;
+    private String Aches;
+    private String Contact;
+    private String Date;
+    private String Time;
+
+
+    public Screening() {
+    }
+
+
+    public String getDrycough() {
+        return drycough;
+    }
+
+    public void setDrycough(String drycough) {
+        this.drycough = drycough;
+    }
+
+    public String getFever() {
+        return Fever;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getTime() {
+        return Time;
+    }
+
+    public void setTime(String time) {
+        Time = time;
+    }
+
+    public void setFever(String fever) {
+        Fever = fever;
+    }
+
+    public String getFatigue() {
+        return Fatigue;
+    }
+
+    public void setFatigue(String fatigue) {
+        Fatigue = fatigue;
+    }
+
+    public String getHeadache() {
+        return Headache;
+    }
+
+    public void setHeadache(String headache) {
+        Headache = headache;
+    }
+
+    public String getSorethroat() {
+        return Sorethroat;
+    }
+
+    public void setSorethroat(String sorethroat) {
+        Sorethroat = sorethroat;
+    }
+
+    public String getTaste() {
+        return Taste;
+    }
+
+    public void setTaste(String taste) {
+        Taste = taste;
+    }
+
+    public String getAches() {
+        return Aches;
+    }
+
+    public void setAches(String aches) {
+        Aches = aches;
+    }
+
+    public String getContact() {
+        return Contact;
+    }
+
+    public void setContact(String contact) {
+        Contact = contact;
+    }
+}
